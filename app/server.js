2193a2a792bd6167e88ec936583ce4c6bad77ca6
alexPietro <alexpietro.public@gmail.com>
2025-05-28 08:24:47 -0300

    Sofrendo :/

--- app/server.js
+++ app/server.js
@@ -1,8 +1,12 @@
-const express = require("express")
-const path = require("path")
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Middleware para arquivos estáticos
 app.use(express.static(path.join(__dirname, '../client/public')));
@@ -20,9 +24,10 @@ app.post('/contato', (req, res) => {
   res.json({ status: 'sucesso', mensagem: 'Mensagem enviada com sucesso!' });
 });
 
-app.get('/', (req, res)=>{
-  res.sendFile(path.join(__dirname, '../client/public', 'index.html'))
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/public', 'index.html'));
+});
+
 
 // Inicia o servidor
 app.listen(PORT, () => {
