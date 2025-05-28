c9c3cf3806362510153342ea54bd32a469956a4b
alexPietro <alexpietro.public@gmail.com>
2025-05-28 09:13:25 -0300

    Pastas Organizadas

--- app/server.js
+++ app/server.js
@@ -25,7 +25,7 @@ app.post('/contato', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/public/pages', 'index.html'));
 });
 
 
